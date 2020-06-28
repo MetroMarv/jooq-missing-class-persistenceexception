a8dbccb85d8ced1ba756d14dcad9d3068c563e21
Marvin Rensing <marvin.rensing@posteo.de>
2020-06-28 13:48:24 +0200

    Fails with version Java 8 too

--- model/src/main/java/org/example/model/Author.java
+++ model/src/main/java/org/example/model/Author.java
@@ -2,11 +2,17 @@ package org.example.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "author")
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     
     @Column(name="name")
     private String name;
